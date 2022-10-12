dd36f80e93a22d35498d0718ef1c6c94f30a301f
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2022-10-12 11:10:00 +0200

    Added Module3.py with pseudocode

---
+++ Module3.py
@@ -0,0 +1,13 @@
+"""
+Voor layout startscherm zie ('Layout scherm.jpg') en voor layout stationsscherm zie ('Layout2.jpg')
+1.  Maak een keuzemenu met layout startscherm met 3 knoppen
+2.  Open het scherm layout2 met de titel van het station
+2.	Haal de laatste 5 opmerkingen over het station uit de database
+3.	Haal de stationsfaciliteiten van het station uit de database
+6.	Haal de weerinformatie van de stad van het station voor de komende uren
+4.	Laat deze gegevens volgens de layout op het scherm zien
+7.	Herhaal de volgende stappen tot het programma sluit
+8.	Haal elke 5 minuten nieuwe weerinformatie op
+9.	Kijk elke 5 seconden of er nieuwe opmerkingen zijn
+10.	Update de nieuwe informatie op t scherm
+"""
