36068cc9a48ed4ceeefe146d6f2cb2eef70278db
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2022-11-02 10:04:52 +0100

    Added docstring to Module1.py and Module2.py

--- Module1.py
+++ Module1.py
@@ -1,12 +1,29 @@
+"""
+1. Laat de gebruiker een opmerking maken
+2. Kijk of het niet te lang of te kort is
+3. Kies een random station
+4. Laat de gebruiker een naam invoeren
+5. Als er niks wordt ingevoerd of de naam te lang is, vul 'anoniem'in
+6. Haalt de tijd van de opmerking op
+7. Schrijft de volgende data bij in het CSV bestand
+8. Herhaal alle stappen tot het programma wordt gesloten
+"""
+
+# imports
 import random
 import time
 import csv
 import os
 
+# list of possible locations
 locaties = ["Arnhem", "Utrecht", "Den Haag"]
 
+
+# a function the lets the user input a review
 def opmerkingMaken():
+    # input a review
     opmerking = input("Wat vind u van dit station? Max 140 karakters: ")
+    # if too long or too short, give errormessage and try again, otherwise return the review
     if(len(opmerking) > 140):
         print("Sorry, het bericht dat u ingevoerd heeft is te lang, probeer het opnieuw.")
         return opmerkingMaken()
@@ -15,18 +32,32 @@ def opmerkingMaken():
         return opmerkingMaken()
     return opmerking
 
+
+# while the code is running repeat the following
 while True:
+    # choose a random station out of the 3
     station = locaties[random.randint(0, 2)]
+
+    # input a username
     naam = input("Goedendag, wat is uw naam? Om anoniem te blijven vul in 'anoniem': ")
     if(naam == "" or len(naam) > 30):
+        # sets username to anonymous if too short or too long
         naam = "anoniem"
+
+    # calls review function
     opmerking = opmerkingMaken()
+
+    # get time at which the review has been made
     datumTijd = time.strftime('%a %d %b %Y, %H:%M:%S', time.localtime())
+
+    # append review data to the CSV file
     file = open('opmerkingen.csv', 'a')
     writer = csv.writer(file)
     data = [opmerking, datumTijd, naam, station]
     writer.writerow(data)
     file.close()
+
+    #   thank user, wait 5 seconds and clear CLI
     print("Bedankt voor uw feedback, we doen ons best om dagelijks te verbeteren, fijne dag!")
     time.sleep(5)
     os.system('CLS')
--- Module2.py
+++ Module2.py
@@ -1,21 +1,27 @@
 """
-1.	Open het csv bestand met de informatie van de opmerkingen.
-2.	Sla de data op in een 2D lijst.
-3.	Loop door de opmerkingen van de lijst, laat ze 1 voor 1 van oud tot nieuw op het scherm zien.
-4.	Geef per opmerking de gebruiker (de moderator) de optie om het goed of af te keuren.
-5.	Als het goedgekeurd wordt speel stap 6 tm 7 af.
-6.	Sla alle bijbehorende data van de opmerking op plus de datum en tijd van de beoordeling en het email adres van de moderator.
+1.  Laat de moderator inloggen met naam en mailadres, check of het mailadres al eerder is gebruikt, zo niet sla het op in de database
+2.	Open het csv bestand met de informatie van de opmerkingen.
+3.	Sla de data op in een 2D lijst.
+4.	Loop door de opmerkingen van de lijst, laat ze 1 voor 1 van oud tot nieuw op het scherm zien.
+5.	Geef per opmerking de gebruiker (de moderator) de optie om het goed of af te keuren.
+6.	Sla alle bijbehorende data van de opmerking, of het goedgekeurd is of niet op plus de datum en tijd van de beoordeling en het email adres van de moderator.
 7.	Voeg deze data toe aan de postgre sql database.
 8.	Ga door naar de volgende opmerking.
 9.	Na het beoordelen van elke opmerking uit het csv bestand moet het gewist worden.
 """
+# imports
 import csv
 import os
 import time
 import psycopg2
 
+
+# function that checks wether the review has been accepted or rejected
 def goedkeuren():
+    # gets input
     goedkeuring = input("Als het bericht goed wordt gekeurd, typ 'goedgekeurd', anders typ 'afgekeurd': ")
+
+    # checks which of the two options have been chosen, if answer is unclear, repeats function
     if (goedkeuring == "goedgekeurd"):
         return True
     elif (goedkeuring == "afgekeurd"):
@@ -24,17 +30,20 @@ def goedkeuren():
         print("Oordeel niet goed geformeerd, typ aub 'goedgekeurd' of 'afgekeurd'")
         return goedkeuren()
 
+# input moderator logins
 modNaam = input("Goedemiddag moderator van NS, wat is uw naam?: ")
 modMail = input("Wat is uw werkmail?: ")
 print("Dankuwel "+ modNaam + ", werkze!")
+
+# makes connection with the database
 file = open("postrgre_info.txt", "r")
 data = (file.read()).split(";")
 name = data[0]
 ww = data[1]
 connection = psycopg2.connect(user=name, password=ww, host="localhost", database="ProjectZuil")
-
 cursor = connection.cursor()
 
+# checks if mail has been used before
 cursor.execute('SELECT * FROM moderator')
 itterations = 0
 duplicate = False
@@ -43,45 +52,61 @@ for i in cursor.fetchall():
     if (i[0] == modMail):
         duplicate = True
 
+# if mail has not been used before, put the moderator logins in the database
 if itterations == 0 or duplicate == False:
     query = "INSERT INTO moderator (mail, naam) VALUES (%s,%s)"
     data = (modMail, modNaam)
     cursor.execute(query, data)
     connection.commit()
 
+# wait 3 seconds and clear the CLI
 time.sleep(3)
 os.system("CLS")
 
+# opens CSV file and reads the review data
 file = open('opmerkingen.csv', 'r')
 reader = csv.reader(file)
 data = list(reader)
 file.close()
 
+#loops through the list of reviews
 for i in data:
     if(i != []):
+        # shows the review on the screen and calls the moderating function
         print("Het bericht wordt hieronder getoond \n\n'" + i[0] + "'\n")
         goedgekeurd = goedkeuren()
+
+        # confirm the moderators decision
         if (goedgekeurd == True):
             print("\nU heeft het bericht goedgekeurd")
         else:
             print("\nU heeft het bericht afgekeurd")
+
+        # connect to the database
         connection = psycopg2.connect(user=name, password=ww, host="localhost", database="ProjectZuil")
         cursor = connection.cursor()
+
+        # takes the next review id out of the database
         cursor.execute('select * from opmerking')
         opmerkingnr = len(cursor.fetchall())
+
+        # puts all necesarry data in a tuple (reviewID, review, reviewdata, username, station, wether the review has been accepted or not, moderatingdate, moderatormail)
         newData = (opmerkingnr, i[0], i[1], i[2], i[3], goedgekeurd, time.strftime('%a %d %b %Y, %H:%M:%S', time.localtime()), modMail)
 
+        # pushes data to the database
         query = "INSERT INTO opmerking (opmerkingnr, opmerking, datumtijd, gebruikersnaam, stationnaam, goedgekeurd, keurdatumtijd, mail) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
         cursor.execute(query, newData)
 
+        # saves database queries and closes connection
         connection.commit()
-
         cursor.close()
         connection.close()
 
+        # waits 3 seconds and clear CLI
         time.sleep(3)
         os.system('CLS')
 
+# clears CSV file
 file = open('opmerkingen.csv', 'w')
 writer = csv.writer(file)
 writer.writerow([])
