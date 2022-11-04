fe969f77741d58d99fe9148cebfe52a4c04cb970
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2022-11-04 14:54:43 +0100

    working Module2.py gui

--- Module2.py
+++ Module2.py
@@ -8,100 +8,154 @@
 7.	Update de informatie van de desbetreffende opmerking met deze data
 8.	Ga door naar de volgende opmerking.
 """
+
+
 # imports
-import os
-import time
 import psycopg2
+from tkinter import *
+import time
 
 
-# function that checks whether the review has been accepted or rejected
-def approve():
-    # gets input
-    moderation = input("Als het bericht goed wordt gekeurd, typ 'goedgekeurd', anders typ 'afgekeurd': ")
+ns_blue = "#%02x%02x%02x" % (0, 48, 130)
+ns_light_blue = "#%02x%02x%02x" % (200, 220, 255)
 
-    # checks which of the two options have been chosen, if answer is unclear, repeats function
-    if (moderation == "goedgekeurd"):
-        return True
-    elif (moderation == "afgekeurd"):
-        return False
-    else:
-        print("Oordeel niet goed geformeerd, typ aub 'goedgekeurd' of 'afgekeurd'")
-        return approve()
 
-# input moderator logins
-modName = input("Goedemiddag moderator van NS, wat is uw naam?: ")
-modMail = input("Wat is uw werkmail?: ")
-print("Dankuwel "+ modName + ", werkze!")
+# main function
+def main():
 
-# makes connection with the database
-file = open("postrgre_info.txt", "r")
-data = (file.read()).split(";")
-name = data[0]
-password = data[1]
-connection = psycopg2.connect(user=name, password=password, host="localhost", database="ProjectZuil")
-cursor = connection.cursor()
+    # get logins for database
+    file = open("postrgre_info.txt", "r")
+    data = (file.read()).split(";")
+    dbusername = data[0]
+    password = data[1]
 
-# checks if mail has been used before
-cursor.execute('SELECT * FROM moderator')
-itterations = 0
-duplicate = False
-for i in cursor.fetchall():
-    itterations += 1
-    if (i[0] == modMail):
-        duplicate = True
 
-# if mail has not been used before, put the moderator logins in the database
-if itterations == 0 or duplicate == False:
-    query = "INSERT INTO moderator (mail, naam) VALUES (%s,%s)"
-    data = (modMail, modName)
-    cursor.execute(query, data)
-    connection.commit()
+    # clock update function
+    def clock_tick():
+        current_time = time.strftime('%H:%M', time.localtime())
+        clock.config(text=current_time)
+        root.after(1000, clock_tick)
 
-# wait 3 seconds and clear the CLI
-time.sleep(3)
-os.system("CLS")
 
-# makes connection with the database
-connection = psycopg2.connect(user=name, password=password, host="localhost", database="ProjectZuil")
-cursor = connection.cursor()
+    # enter moderatingscreen function
+    def enter_screen():
+        global  approveList, i
 
-# get all unmoderated reviews from the database
-cursor.execute('select * from opmerking where goedgekeurd is null')
-approveList = cursor.fetchall()
+        # makes connection with the database
+        connection = psycopg2.connect(user=dbusername, password=password, host="localhost", database="ProjectZuil")
+        cursor = connection.cursor()
 
-# saves database queries and closes connection
-connection.commit()
-cursor.close()
-connection.close()
+        # get all unmoderated reviews from the database
+        cursor.execute('select * from opmerking where goedgekeurd is null')
+        approveList = cursor.fetchall()
 
-# loop through unmoderated reviews
-for i in approveList:
-    # shows the review on the screen and calls the moderating function
-    print("Het bericht wordt hieronder getoond \n\n'" + i[2] + "'\n")
-    moderation = approve()
+        # saves database queries and closes connection
+        connection.commit()
+        cursor.close()
+        connection.close()
+        i = 0
 
-    # confirm the moderators decision
-    if (moderation == True):
-        print("\nU heeft het bericht goedgekeurd")
-    else:
-        print("\nU heeft het bericht afgekeurd")
+        def next_review(approved):
+            global i
 
-    # get the time of moderation
-    dateTime = time.strftime('%a %d %b %Y, %H:%M:%S', time.localtime())
+            # get the time of moderation
+            dateTime = time.strftime('%a %d %b %Y, %H:%M:%S', time.localtime())
 
-    # make connection with the database
-    connection = psycopg2.connect(user=name, password=password, host="localhost", database="ProjectZuil")
-    cursor = connection.cursor()
+            # make connection with the database
+            connection = psycopg2.connect(user=dbusername, password=password, host="localhost", database="ProjectZuil")
+            cursor = connection.cursor()
 
-    # updates reviewdata with moderatingdata
-    query = 'update opmerking set goedgekeurd = %s, keurdatumtijd = %s, mail = %s where opmerkingnr = %s'
-    cursor.execute(query, (moderation, dateTime, modMail, i[0]))
+            # updates reviewdata with moderatingdata
+            print(i)
+            query = 'update opmerking set goedgekeurd = %s, keurdatumtijd = %s, mail = %s where opmerkingnr = %s'
+            cursor.execute(query, (approved, dateTime, modMail, approveList[i][0]))
 
-    # saves database queries and closes connection
-    connection.commit()
-    cursor.close()
-    connection.close()
+            # saves database queries and closes connection
+            connection.commit()
+            cursor.close()
+            connection.close()
 
-    # waits 3 seconds and clear CLI
-    time.sleep(3)
-    os.system('CLS')
+            i += 1
+            if len(approveList) > i:
+                review.config(text=approveList[i][2])
+            else:
+                goed.destroy()
+                nietgoed.destroy()
+                review.destroy()
+                canvas.create_text(640, 360, text="Dat waren de opmerkingen, bedankt!", anchor='center', font='Sans 50 bold', fill=ns_blue)
+                root.after(3000, exit)
+
+        if name.get() != "" and mail.get() != "":
+            modName = name.get()
+            modMail = mail.get()
+
+            canvas.delete('start')
+            name.destroy()
+            mail.destroy()
+            login.destroy()
+            # make connection with the database
+            connection = psycopg2.connect(user=dbusername, password=password, host="localhost", database="ProjectZuil")
+            cursor = connection.cursor()
+
+            # checks if mail has been used before
+            cursor.execute('SELECT * FROM moderator')
+            itterations = 0
+            duplicate = False
+            for data in cursor.fetchall():
+                itterations += 1
+                if (data[0] == modMail):
+                    duplicate = True
+
+            # if mail has not been used before, put the moderator logins in the database
+            if itterations == 0 or duplicate == False:
+                query = "INSERT INTO moderator (mail, naam) VALUES (%s,%s)"
+                data = (modMail, modName)
+                cursor.execute(query, data)
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+
+
+            review = Label(canvas, text=approveList[0][2], font=('Sans 50 bold'), fg=ns_blue)
+            review.place(x=640, y=250, anchor='n')
+
+            goed = Button(canvas, text="goedgekeurd", command= lambda: next_review(True), bd=4, fg=ns_blue, font="Sans 30 bold")
+            goed.place(x=330, y=450)
+
+            nietgoed = Button(canvas, text="afgekeurd", padx=35, command=lambda: next_review(False), bd=4, fg=ns_blue, font="Sans 30 bold")
+            nietgoed.place(x=675, y=450)
+
+
+
+    root = Tk()
+
+    canvas = Canvas(root, height=720, width=1280)
+
+    canvas.create_rectangle(0, 0, 1280, 100, fill=(ns_blue))
+    canvas.create_rectangle(0, 620, 1280, 720, fill=(ns_blue))
+    canvas.create_text(640, 180, text="Goedendag moderator van NS", anchor='n', font='Sans 40 bold', fill=ns_blue, tag='start')
+    canvas.create_text(420, 310, text='Naam:', font='Sans 20', fill=ns_blue, anchor='nw', tag='start')
+    canvas.create_text(420, 400, text='Mail:', font='Sans 20', fill=ns_blue, anchor='nw', tag='start')
+
+    name = Entry(canvas, font='Sans 20', fg=ns_blue, justify='center', width=20)
+    name.place(x = 510, y= 310)
+
+    mail = Entry(canvas, font='Sans 20', fg=ns_blue, justify='center', width=20)
+    mail.place(x=510, y=400)
+
+    login = Button(canvas, text="Log in", command=enter_screen, bd=4, fg=ns_blue,font="Sans 20")
+    login.place(x=600, y=500)
+
+    canvas.focus_set()
+
+    # create a clock
+    clock = Label(canvas, text="", font=('Sans 50 bold'), bg=ns_blue, fg="white")
+    clock.place(x=1100, y=10)
+    clock_tick()
+
+    canvas.pack()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
